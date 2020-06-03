5d1851edc1eeeda6ef550f6fd63515f9aa62d16e
Mateusz <m.gruzla@hotmail.com>
2020-06-04 00:49:07 +0200

    Cloud function set up.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,74 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class LambdaDemo extends Component {
   constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+    super(props);
+    this.state = { loading: false, msg: null };
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
+  // fetch('https://example.com/profile', {
+  //   method: 'POST', // or 'PUT'
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(data),
+  // })
+  // .then(response => response.json())
+  // .then(data => {
+  //   console.log('Success:', data);
+  // })
+  // .catch((error) => {
+  //   console.error('Error:', error);
+  // });
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+  handleClick = (api) => (e) => {
+    e.preventDefault();
 
+    this.setState({ loading: true });
+    fetch('/.netlify/functions/' + api)
+      .then((response) => response.json())
+      .then((json) => this.setState({ loading: false, msg: json.msg }));
+  };
+
+  // this.setState({ loading: false, msg: json.msg })
   render() {
-    const { loading, msg } = this.state
+    const { loading, msg } = this.state;
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        {/* <button onClick={this.handleClick('hello')}>
+          {loading ? 'Loading...' : 'Call Lambda'}
+        </button>
+        <button onClick={this.handleClick('async-dadjoke')}>
+          {loading ? 'Loading...' : 'Call Async Lambda'}
+        </button> */}
+        <input id='vatInput' type='text' />
+        <button onClick={this.handleClick('verify')}>
+          {loading ? 'Loading...' : 'Sprawdź NIP'}
+        </button>
         <br />
         <span>{msg}</span>
       </p>
-    )
+    );
   }
 }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <LambdaDemo />
         </header>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/lambda/verify.js
@@ -0,0 +1,26 @@
+// this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
+const validate = require('validate-vat');
+
+export function handler(event, context, callback) {
+  console.log('queryStringParameters', event.queryStringParameters);
+  const elo = async (err, info) => {
+    try {
+      const data = await info;
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          msg: `
+          NIP: ${data.vatNumber},
+          Fimra: ${data.name},
+          adres: ${data.address},
+          VAT aktywny: ${data.valid ? 'TAK' : 'NIE'},
+          `,
+        }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  validate('PL', `8822119889`, elo);
+}
